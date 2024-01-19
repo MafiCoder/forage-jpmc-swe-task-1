f2ef7241fcb98a3bf33ae0fb7aa745ed64491736
MafiCoder <salesforcemimi@gmail.com>
2024-01-19 14:07:17 -0800

    update to satisfy the requirements

--- client3.py
+++ client3.py
@@ -29,20 +29,23 @@ QUERY = "http://localhost:8080/query?id={}"
 N = 500
 
 
-def getDataPoint(quote):
+def get_data_point(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
-def getRatio(price_a, price_b):
+def get_ratio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return
+    ratio = price_a/price_b
+    return ratio
 
 
 # Main
@@ -52,8 +55,13 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
+            stock, bid_price, ask_price, price = get_data_point(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+        stock_names = list(prices.keys())
+        print("Ratio %s" % get_ratio(prices[stock_names[0]], prices[stock_names[1]]))
 
-        print("Ratio %s" % getRatio(price, price))
+
+
